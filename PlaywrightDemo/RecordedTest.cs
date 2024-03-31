d57c5248003ae879c0c6f713209f5a682cdd0f01
shust <shust@DESKTOP-6GNUVDA>
2024-03-31 13:41:48 -0600

    Add project files.

---
+++ PlaywrightDemo/RecordedTest.cs
@@ -0,0 +1,18 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class RecordedTests : PageTest
+{
+    [Test]
+    public async Task TestThatIHaveRecorded()
+    {
+        await Page.GotoAsync("https://demo.playwright.dev/todomvc/#/active");
+    }
+}
